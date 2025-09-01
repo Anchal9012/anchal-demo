ad33daeb17c9000665cead32cdcac466a2d04eb2
Anchal <ranakotianchal@gmail.com>
2025-09-01 19:42:31 +0530

    Adding New Programs.

---
+++ main02.cpp
@@ -0,0 +1,8 @@
+#include <iostream> 
+using namespace std;
+int main(){
+    //camel case notation
+    int MarksInMath =70;
+cout<<"Your marks in math is" << MarksInMath;
+return 0;
+}
---
+++ main06.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int age;
+    cout<<"Enter your age "<<endl;
+    cin>>age ;
+    if (age>150|| age<1){
+        cout<<"invalid age"<<endl;
+    }
+    else if( age>18){
+        cout<<"You can vote"<<endl;
+
+    }else 
+    cout<<"You cannot vote"<<endl;
+    return 0;
+}
